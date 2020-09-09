5a643b10ee591beb34f667710cfd7ba04ae55a5a
ThangVM <data@htauto.com.vn>
2020-09-09 11:24:20 +0700

    schedule spring anotation

--- src/main/java/com/fastwok/crawler/CrawlerFastworkApplication.java
+++ src/main/java/com/fastwok/crawler/CrawlerFastworkApplication.java
@@ -1,9 +1,14 @@
 package com.fastwok.crawler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableScheduling
 public class CrawlerFastworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(CrawlerFastworkApplication.class, args);
