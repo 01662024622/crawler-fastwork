7a63ab63625247e779bfb87ddc22b5cc867b788f
ThangVM <data@htauto.com.vn>
2020-09-10 14:10:23 +0700

    fix database connect

--- src/main/java/com/fastwok/crawler/repository/CustomerFastworkRepository.java
+++ src/main/java/com/fastwok/crawler/repository/CustomerFastworkRepository.java
@@ -4,6 +4,7 @@ import com.fastwok.crawler.entities.CustomerFastwork;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+
 public interface CustomerFastworkRepository extends JpaRepository<CustomerFastwork, Long> {
     @Query("SELECT cus FROM CustomerFastwork cus WHERE cus.fw_id=?1")
     CustomerFastwork getByIdFastwork(String id);
--- src/main/java/com/fastwok/crawler/repository/CustomerRepository.java
+++ src/main/java/com/fastwok/crawler/repository/CustomerRepository.java
@@ -3,9 +3,9 @@ package com.fastwok.crawler.repository;
 import com.fastwok.crawler.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
