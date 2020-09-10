41e09d3aea22172e7b0d9028982bb9bd3ec96ea5
ThangVM <data@htauto.com.vn>
2020-09-10 14:30:19 +0700

    fix main

--- src/main/java/com/fastwok/crawler/CrawlerFastworkApplication.java
+++ src/main/java/com/fastwok/crawler/CrawlerFastworkApplication.java
@@ -3,11 +3,16 @@ package com.fastwok.crawler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.fastwok.crawler.entities"})
+@EnableJpaRepositories(basePackages = {"com.fastwok.crawler.repository"})
 @EnableTransactionManagement
+@Slf4j
 @EnableScheduling
 public class CrawlerFastworkApplication {
     public static void main(String[] args) {
